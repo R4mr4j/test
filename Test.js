9f4e40d8fa7d07765ab8563808f7d15527878179
R9mr9j kum9w9t <132282378+R4mr4j@users.noreply.github.com>
2024-05-28 11:50:34 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,18 +1,12 @@
 const fs = require('fs');
 const prompt = require('prompt');
+const fetch = require('node-fetch'); // Import the fetch module
 const login = require('facebook-chat-api');
 const chalk = require('chalk');
 
 prompt.message = '';
 
 prompt.start();
-
-var msg = {
-    body: "",
-    attachment: fs.createReadStream(__dirname + '/sk.gif')
-};
-
-console.log(chalk.bold.hex("#00FF00").bold(" "));
 
 prompt.get(['password', 'apstatefile', 'targetID', 'timer'], function (err, result) {
     if (err) { return onErr(err); }
@@ -25,23 +19,33 @@ prompt.get(['password', 'apstatefile', 'targetID', 'timer'], function (err, resu
                 console.log('[x] Your password is incorrect! Please enter the correct password.');
                 process.exit();
             }
+        })
+        .catch(err => {
+            console.error('Error fetching data:', err);
+            process.exit(1);
         });
 
-    const appState = JSON.parse(fs.readFileSync(result.apstatefile, 'utf8'));
+    const appState = JSON.parse(fs.readFileSync(result.apstatefile, 'utf8'), (err, data) => {
+        if (err) {
+            console.error('Error reading appState file:', err);
+            process.exit(1);
+        }
+        return data;
+    });
+
     login({ appState }, (err, api) => {
         if (err) return console.error(err);
         setInterval(() => {
-        	var msg = {
-            body: "",
-            attachment: fs.createReadStream(__dirname + '/sk.gif')
-        };
-            api.sendMessage(msg, targetID);
-        }, result.targetID, () => {
+            var msg = {
+                body: "",
+                attachment: fs.createReadStream(__dirname + '/sk.gif')
+            };
+            api.sendMessage(msg, result.targetID); // Use result.targetID instead of targetID
+        }, result.timer * 1000, () => {
             const timestamp = new Date().toLocaleString();
             console.log(`\n\x1b[32mImage sent successfully at ${timestamp}.`);
         });
-    }, result.timer * 1000);
-
+    });
 });
 
 function onErr(err) {
